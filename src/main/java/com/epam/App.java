ec739016dd249d5b9be639544aa40c57399097f1
Volodymyr Ostapiuk <Volodymyr_Ostapiuk@epam.com>
2020-06-23 01:14:44 +0300

    Added model for Student entity
    Added model for Student entity and updated dependencies

--- src/main/java/com/epam/App.java
+++ src/main/java/com/epam/App.java
@@ -1,7 +1,7 @@
 package com.epam;
 
 public class App {
-        public static void main(String[] args) {
-                
-        }
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/main/java/com/epam/model/StudentEntity.java
@@ -0,0 +1,82 @@
+package com.epam.model;
+
+import java.sql.Date;
+
+public class StudentEntity {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private Date birthday;
+    private int phone;
+    private int classID;
+
+    public StudentEntity() {
+    }
+
+    public StudentEntity(int id, String firstName, String lastName, String address, Date birthday, int phone, int classID) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.birthday = birthday;
+        this.phone = phone;
+        this.classID = classID;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public int getClassID() {
+        return classID;
+    }
+
+    public void setClassID(int classID) {
+        this.classID = classID;
+    }
+}
