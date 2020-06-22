056176b16b2897d5042add27b02c2e2bde6ed116
Volodymyr Ostapiuk <Volodymyr_Ostapiuk@epam.com>
2020-06-23 01:40:23 +0300

    Added model for Lesson entity

---
+++ src/main/java/com/epam/model/LessonEntity.java
@@ -0,0 +1,40 @@
+package com.epam.model;
+
+public class LessonEntity {
+    private int id;
+    private String name;
+    private String description;
+
+    public LessonEntity() {
+    }
+
+    public LessonEntity(int id, String number, String description) {
+        this.id = id;
+        this.name = number;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return name;
+    }
+
+    public void setNumber(String number) {
+        this.name = number;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
