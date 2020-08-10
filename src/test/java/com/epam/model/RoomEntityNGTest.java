eed3054b81fe92376d582182da93b65a50efa1e5
Volodymyr Ostapiuk <Volodymyr_Ostapiuk@epam.com>
2020-08-10 13:17:14 +0300

    Updated testNG classes

--- src/test/java/com/epam/model/RoomEntityNGTest.java
+++ src/test/java/com/epam/model/RoomEntityNGTest.java
@@ -1,4 +1,102 @@
 package com.epam.model;
 
+import java.lang.reflect.Field;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class RoomEntityNGTest {
+    @Test(groups = { "functest" })
+    public void testIdSetter() throws NoSuchFieldException, IllegalAccessException {
+        final RoomEntity roomEntity = new RoomEntity();
+        roomEntity.setId(101);
+        final Field field = roomEntity.getClass().getDeclaredField("id");
+        field.setAccessible(true);
+        Assert.assertEquals(field.get(roomEntity), 101, "Fields didn't match");
+        Assert.assertNotEquals(field.get(roomEntity), 111, "Fields match");
+    }
+
+    @Test(groups = { "functest" })
+    public void testIdGetter() throws NoSuchFieldException, IllegalAccessException {
+        final RoomEntity roomEntity = new RoomEntity();
+        final Field field = roomEntity.getClass().getDeclaredField("id");
+        field.setAccessible(true);
+        field.set(roomEntity, 55);
+        final int result = roomEntity.getId();
+        Assert.assertEquals(result, 55, "Field wasn't retrieved properly");
+        Assert.assertNotEquals(result, 53, "Field was retrieved properly");
+    }
+
+    @Test(groups = { "functest" })
+    public void testNumberSetter() throws NoSuchFieldException, IllegalAccessException {
+        final RoomEntity roomEntity = new RoomEntity();
+        roomEntity.setNumber("102D");
+        final Field field = roomEntity.getClass().getDeclaredField("number");
+        field.setAccessible(true);
+        Assert.assertEquals(field.get(roomEntity), "102D", "Fields didn't match");
+        Assert.assertNotEquals(field.get(roomEntity), "102", "Fields match");
+    }
+
+    @Test(groups = { "functest" })
+    public void testNumberGetter() throws NoSuchFieldException, IllegalAccessException {
+        final RoomEntity roomEntity = new RoomEntity();
+        final Field field = roomEntity.getClass().getDeclaredField("number");
+        field.setAccessible(true);
+        field.set(roomEntity, "403");
+        final String result = roomEntity.getNumber();
+        Assert.assertEquals(result, "403", "Field wasn't retrieved properly");
+        Assert.assertNotEquals(result, "403A", "Field was retrieved properly");
+    }
+
+    @Test(groups = { "functest" })
+    public void testDescriptionSetter() throws NoSuchFieldException, IllegalAccessException {
+        final RoomEntity roomEntity = new RoomEntity();
+        roomEntity.setDescription("Room is situated on 3rd floor");
+        final Field field = roomEntity.getClass().getDeclaredField("description");
+        field.setAccessible(true);
+        Assert.assertEquals(field.get(roomEntity),
+                "Room is situated on 3rd floor", "Fields didn't match");
+        Assert.assertNotEquals(field.get(roomEntity),
+                "Room is situated on 2nd floor", "Fields match");
+    }
+
+    @Test(groups = { "functest" })
+    public void testDescriptionGetter() throws NoSuchFieldException, IllegalAccessException {
+        final RoomEntity roomEntity = new RoomEntity();
+        final Field field = roomEntity.getClass().getDeclaredField("description");
+        field.setAccessible(true);
+        field.set(roomEntity, "Room has 15 desks");
+        final String result = roomEntity.getDescription();
+        Assert.assertEquals(result, "Room has 15 desks", "Field wasn't retrieved properly");
+        Assert.assertNotEquals(result, "Room has 13 desks", "Field was retrieved properly");
+    }
+
+    @Test(groups = { "checkintest" })
+    public void testToString() {
+        final RoomEntity roomEntity = new RoomEntity();
+        String toString = roomEntity.toString();
+        Assert.assertTrue(toString.contains("id=" + roomEntity.getId()));
+    }
+
+    @Test(groups = { "functest", "checkintest" })
+    public void testClassEntity1() {
+        final RoomEntity roomEntity = new RoomEntity();
+        Assert.assertEquals(0, roomEntity.getId());
+        Assert.assertNull(roomEntity.getDescription());
+    }
+
+    @Test(groups = { "functest", "checkintest" })
+    public void testClassEntity2() {
+        final RoomEntity roomEntity = new RoomEntity("102B", "Room has 15 desks");
+        Assert.assertEquals("102B", roomEntity.getNumber());
+        Assert.assertNotEquals(null, roomEntity.getDescription());
+    }
+
+    @Test(groups = { "functest", "checkintest" })
+    public void testClassEntity3() {
+        final RoomEntity roomEntity = new RoomEntity(1, "118A", "Room is very cold");
+        Assert.assertEquals("118A", roomEntity.getNumber());
+        Assert.assertEquals(1, roomEntity.getId());
+        Assert.assertNotEquals(null, roomEntity.getDescription());
+    }
 }
+
--- src/test/java/com/epam/model/SubjectEntityNGTest.java
+++ src/test/java/com/epam/model/SubjectEntityNGTest.java
@@ -1,4 +1,103 @@
 package com.epam.model;
 
+import java.lang.reflect.Field;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class SubjectEntityNGTest {
+    @Test(groups = { "functest" })
+    public void testIdSetter() throws NoSuchFieldException, IllegalAccessException {
+        final SubjectEntity subjectEntity = new SubjectEntity();
+        subjectEntity.setId(101);
+        final Field field = subjectEntity.getClass().getDeclaredField("id");
+        field.setAccessible(true);
+        Assert.assertEquals(field.get(subjectEntity), 101, "Fields didn't match");
+        Assert.assertNotEquals(field.get(subjectEntity), 111, "Fields match");
+    }
+
+    @Test(groups = { "functest" })
+    public void testIdGetter() throws NoSuchFieldException, IllegalAccessException {
+        final SubjectEntity subjectEntity = new SubjectEntity();
+        final Field field = subjectEntity.getClass().getDeclaredField("id");
+        field.setAccessible(true);
+        field.set(subjectEntity, 55);
+        final int result = subjectEntity.getId();
+        Assert.assertEquals(result, 55, "Field wasn't retrieved properly");
+        Assert.assertNotEquals(result, 53, "Field was retrieved properly");
+    }
+
+    @Test(groups = { "functest" })
+    public void testNameSetter() throws NoSuchFieldException, IllegalAccessException {
+        final SubjectEntity subjectEntity = new SubjectEntity();
+        subjectEntity.setName("Math");
+        final Field field = subjectEntity.getClass().getDeclaredField("name");
+        field.setAccessible(true);
+        Assert.assertEquals(field.get(subjectEntity), "Math", "Fields didn't match");
+        Assert.assertNotEquals(field.get(subjectEntity), "Algebra", "Fields match");
+    }
+
+    @Test(groups = { "functest" })
+    public void testNameGetter() throws NoSuchFieldException, IllegalAccessException {
+        final SubjectEntity subjectEntity = new SubjectEntity();
+        final Field field = subjectEntity.getClass().getDeclaredField("name");
+        field.setAccessible(true);
+        field.set(subjectEntity, "English");
+        final String result = subjectEntity.getName();
+        Assert.assertEquals(result, "English", "Field wasn't retrieved properly");
+        Assert.assertNotEquals(result, "British", "Field was retrieved properly");
+    }
+
+    @Test(groups = { "functest" })
+    public void testDescriptionSetter() throws NoSuchFieldException, IllegalAccessException {
+        final SubjectEntity subjectEntity = new SubjectEntity();
+        subjectEntity.setDescription("Subject is from 5th grade");
+        final Field field = subjectEntity.getClass().getDeclaredField("description");
+        field.setAccessible(true);
+        Assert.assertEquals(field.get(subjectEntity),
+                "Subject is from 5th grade", "Fields didn't match");
+        Assert.assertNotEquals(field.get(subjectEntity),
+                "Subject is from 8th grade", "Fields match");
+    }
+
+    @Test(groups = { "functest" })
+    public void testDescriptionGetter() throws NoSuchFieldException, IllegalAccessException {
+        final SubjectEntity subjectEntity = new SubjectEntity();
+        final Field field = subjectEntity.getClass().getDeclaredField("description");
+        field.setAccessible(true);
+        field.set(subjectEntity, "Subject has 2 teachers");
+        final String result = subjectEntity.getDescription();
+        Assert.assertEquals(result, "Subject has 2 teachers", "Field wasn't retrieved properly");
+        Assert.assertNotEquals(result, "Subject has 3 teachers", "Field was retrieved properly");
+    }
+
+    @Test(groups = { "checkintest" })
+    public void testToString() {
+        final SubjectEntity subjectEntity = new SubjectEntity();
+        String toString = subjectEntity.toString();
+        Assert.assertTrue(toString.contains("id=" + subjectEntity.getId()));
+    }
+
+    @Test(groups = { "functest", "checkintest" })
+    public void testClassEntity1() {
+        final SubjectEntity subjectEntity = new SubjectEntity();
+        Assert.assertEquals(0, subjectEntity.getId());
+        Assert.assertNull(subjectEntity.getDescription());
+    }
+
+    @Test(groups = { "functest", "checkintest" })
+    public void testClassEntity2() {
+        final SubjectEntity subjectEntity = new SubjectEntity("Algebra",
+                "Subject is from 5th grade");
+        Assert.assertEquals("Algebra", subjectEntity.getName());
+        Assert.assertNotEquals(null, subjectEntity.getDescription());
+    }
+
+    @Test(groups = { "functest", "checkintest" })
+    public void testClassEntity3() {
+        final SubjectEntity subjectEntity = new SubjectEntity(1, "Astronomy",
+                "Subject is from 10th grade");
+        Assert.assertEquals("Astronomy", subjectEntity.getName());
+        Assert.assertEquals(1, subjectEntity.getId());
+        Assert.assertNotEquals(null, subjectEntity.getDescription());
+    }
 }
